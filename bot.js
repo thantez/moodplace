eabcdd4a8e1f75b8995270598aad5f6ad91e1cc1
Mohammad Mahdi Bakhshi <bakhshi@protonmail.com>
2021-07-19 18:55:49 +0430

    I develope every thing and push them all. I use git just for fun :)).

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const {
   Telegraf,
   Markup,
@@ -5,34 +6,133 @@ const {
   Scenes,
   session,
 } = require('telegraf')
+const { route_calculator } = require('./route')
+
+const BOT_TOKEN = process.env.BOT_TOKEN
+const MAP_TOKEN = process.env.MAP_TOKEN
+const MAP_BASE_URL = 'https://api.geoapify.com/v2' 
+const MAP_PLACE_API = MAP_BASE_URL+'/places?bias=proximity:51.6672222,32.7038826&lang=fa&limit=10&apiKey='+MAP_TOKEN
 
 const OPTIONS_KEYBOARD = Markup.inlineKeyboard([
-  Markup.button.callback('به شدت', 'extremely'),
-  Markup.button.callback('کمی زیاد', 'quite a bit'),
-  Markup.button.callback('متوسط', 'moderately'),
-  Markup.button.callback('کم', 'a little'),
-  Markup.button.callback('خیلی کم یا کلا به هیچ مقدار', 'zero'),
+  [
+    Markup.button.callback('کمی زیاد', 'quite a bit'),
+    Markup.button.callback('به شدت', 'extremely'),
+  ],
+  [
+    Markup.button.callback('متوسط', 'moderately'),
+  ],
+  [
+    Markup.button.callback('خیلی کم یا اصلا', 'zero'),
+    Markup.button.callback('کم', 'a little'),
+  ],    
 ])
 
 const questions = [
-  'q1',
-  'q2',
-  'q3',
+  ['۱' ,'احساس می‌کنم به چیزی علاقه‌مند شده‌ام 😍.', 1],
+  ['۲' ,'پریشان هستم 😰.', -1],
+  ['۳' ,'هیجان‌زده هستم 🤩.', 1],
+  ['۴' ,'از چیزی ناراحتم 😔.', -1],
+  ['۵' ,'احساس قدرت می‌کنم 💪.', 1],
+  ['۶' ,'احساس گناه می‌کنم 😞.', -1],
+  ['۷' ,'ترسیده‌ام 😱.', -1],
+  ['۸' ,'از چیزی متنفرم 🤢.', -1],
+  ['۹' ,'برای کاری مشتاق هستم، حس اشتیاق دارم 😃.', 1],
+  ['۱۰' ,'احساس غرور می‌کنم 😎.', 1],
+  ['۱۱' ,'کج‌خلق و تندخو هستم 😒.', -1],
+  ['۱۲' ,'گوش‌به‌زنگم، مراقب هستم 👀.', 1],
+  ['۱۳' ,'شرمگینم 😓.', -1],
+  ['۱۴' ,'الان طوری هستم که گویی به من الهام شده‌است 😲.', 1],
+  ['۱۵' ,'عصبانی هستم، اعصاب ندارم 😡.', -1],
+  ['۱۶' ,'عزمی راسخ دارم، مصمم هستم 😌.', 1],
+  ['۱۷' ,'نسبت به کاری که می‌کنم مراقب و مواظب هستم 😯.', 1],
+  ['۱۸' ,'اضطراب دارم 😥.', -1],
+  ['۱۹' ,'پر کار و پر تحرک هستم 🥵.', 1],
+  ['۲۰' ,'حس وحشت دارم 😨.', -1],
 ]
 
-const answered_point = point => (ctx, next) => {
-  ctx.session.value.push(point)
-  const step_number = ctx.session.value.length
-  if(step_number == questions.length){
-    ctx.deleteMessage()
-    return ctx.reply('done')
-  }
-  // const message_reference = ctx.session.message_id
-  ctx.editMessageText(questions[step_number], OPTIONS_KEYBOARD)
-  return next()
+const POSITIVE_PLACES = [
+  'education',
+  'sport',
+  'building.place_of_worship',
+  'building.holiday_house',
+  'tourism',
+  'catering',
+  'commercial.food_and_drink',
+  'commercial.hobby',
+  'commercial.gift_and_souvenir',
+]
+
+const MOD_POSITIVE_PLACES = [
+  'building.entertainment',
+  'entertainment',
+  'entertainment.escape_game',
+  'entertainment.cinema',
+  'entertainment.amusement_arcade',
+  'leisure',
+  'service.beauty',
+  'building.place_of_worship',
+  'childcare',
+]
+
+const MEDIUM_PLACES = [
+  'entertainment.escape_game',
+  'entertainment.cinema',
+  'entertainment.amusement_arcade',
+  'leisure',
+  'commercial.shopping_mall',
+  'commercial.books',
+  'commercial.smoking',
+]
+
+const MOD_NEGATIVE_PLACES = [
+  'entertainment.museum',
+  'entertainment.zoo',
+  'entertainment.aquarium',
+  'entertainment.water_park',
+  'childcare',
+  'natural',
+  'building.place_of_worship',
+  'building.holiday_house',
+]
+
+const NEGATIVE_PLACES = [
+  'commercial.shopping_mall',
+  'commercial.books',
+  'commercial.smoking',
+  'catering',
+  'healthcare.clinic_or_praxis',
+  'leisure',
+  'natural',
+  'heritage',
+]
+
+const shuffle = array => {
+  console.log('shuffle: '+array.length)
+  return array.sort(() => Math.random() - 0.5);
 }
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const answered_point = point => ctx => {
+  const step_number = ctx.session.value.length + 1
+  ctx.session.value.push(point * questions[step_number - 1][2])
+  if(step_number == questions.length){
+    ctx.deleteMessage()
+    const values = ctx.session.value
+    const positives = values.filter(v => v > 0).reduce((acc, cv) => acc + cv, 0)
+    const negatives = values.filter(v => v < 0).reduce((acc, cv) => acc + cv, 0) * -1
+    ctx.session.result = positives - negatives
+    const result = (positives - negatives) >= 0? 'خوب' : 'بد'
+    return ctx.replyWithPhoto({
+        source: 'Thermometer.jpg',
+      }, {
+        caption: `پرسشنامه تکمیل شد. مجموع امتیازات احساسات مثبت شما برابر با ${positives} و مجموع احساسات منفی شما برابر با ${negatives} است. مطابق با امتیازهای به‌دست‌آمده حال شما ${result} است. بر اساس همین وضعیت این ربات به شما مکان‌هایی را پیشنهاد خواهد داد.`,
+      }).then(() => {
+        ctx.reply('لطفا از طریق ارسال مکان تلگرام، مکان فعلی خود را ارسال کنید تا نزدیک‌ترین مکان‌ها مطابق با وضعیت احساسات شما برایتان ارسال گردد.')
+      })
+  }
+  ctx.editMessageText(`حس ${questions[step_number][0]}. ${questions[step_number][1]}`, OPTIONS_KEYBOARD)
+}
+
+const bot = new Telegraf(BOT_TOKEN)
 bot.use(session())
 
 bot.command('start', async (ctx) => {
@@ -40,7 +140,7 @@ bot.command('start', async (ctx) => {
     state: 'init',
   }
   await ctx.reply(
-    'سلام. به این بات خوش آمدید. گزینه مورد نظر را انتخاب فرمایید.',
+    'سلام. به ربات پیشنهاد مکان بر اساس احساسات خوش آمدید. لطفا از طریق منوی پایین، گزینه مورد نظر خود را انتخاب فرمایید.',
     Markup.keyboard([
       Markup.button.text('تشخیص احساسات با صوت'),
       Markup.button.text('تشخیص احساسات با پرسشنامه'),
@@ -54,22 +154,90 @@ bot.hears('تشخیص احساسات با پرسشنامه', async (ctx) => {
     state: 'quiz',
     value: [],
   }
-  const sended_first_question = await ctx.reply(questions[0], OPTIONS_KEYBOARD)
-  ctx.session.message_id = sended_first_question.message_id
+  await ctx.reply('لطفا احساسات فعلی خود را نسبت به هر یک از مواردی که مشخص شده‌است از طریق دکمه‌های زیر پیام، بیان کنید.', Markup.removeKeyboard())
+  await ctx.reply(`حس ${questions[0][0]}. ${questions[0][1]}`, OPTIONS_KEYBOARD.resize())
 })
 
-
-console.log(answered_point(0))
 bot.action('extremely', answered_point(5))
 bot.action('quite a bit', answered_point(4))
 bot.action('moderately', answered_point(3))
 bot.action('a little', answered_point(2))
 bot.action('zero', answered_point(1))
 
-bot.on('message', async (ctx) => {
-  console.log('hoy')
+const map_regex = new RegExp('map_.*$')
+bot.action(map_regex, async (ctx) => {
+  const data = ctx.update.callback_query.data
+  const [_, lon, lat, user_lon, user_lat] = data.split('_')
+  const loc = `${lon}_${lat}_${user_lon}_${user_lat}`
+  for (const place_message of ctx.session.place_messages){
+    await ctx.telegram.deleteMessage(ctx.update.callback_query.from.id, place_message)
+  }
+  await ctx.replyWithLocation(lat, lon)
+  await ctx.reply(
+    'با چه وسیله‌ای مسیریابی انجام شود؟',
+    Markup.inlineKeyboard([
+      Markup.button.callback('ماشین', `rd_${loc}`),
+      Markup.button.callback('دوچرخه', `rb_${loc}`),
+      Markup.button.callback('پیاده', `rw_${loc}`),
+    ])
+  )
 })
 
+const route_regex = new RegExp('r._.*$')
+bot.action(route_regex, async (ctx) => {
+  const data = ctx.update.callback_query.data
+  const [route_mode, lon, lat, user_lon, user_lat] = data.split('_')
+  const mode = (route_mode == 'rd' ? 'car' : (route_mode == 'rb' ? 'bike' : 'foot'))
+  await route_calculator(user_lon, user_lat, lon, lat, mode, ctx)
+})
+
+bot.on('location', async (ctx) => {
+  const lat = ctx.message.location.latitude
+  const lon = ctx.message.location.longitude
+  if (!ctx.session || ctx.session.result === undefined)
+    return await ctx.reply('ابتدا باید وضعیت احساسات فعلی شما مشخص شود. برای شروع /start کنید.')
+  const result = ctx.session.result
+  const places = result > 24 ? POSITIVE_PLACES
+    : result <= 24 && result > 8 ? MOD_POSITIVE_PLACES
+    : result <= 8 && result > -8 ? MEDIUM_PLACES
+    : result <= -8 && result > -24 ? MOD_NEGATIVE_PLACES
+    : NEGATIVE_PLACES
+
+  console.log(places)
+  const urls = places.map(place => axios.get(MAP_PLACE_API+`&categories=${place}&filter=circle:${lon},${lat},6000`))
+  try {
+    const res = await axios.all(urls)
+    const features = res.map(json => json.data.features)
+    const flatten_items = features.flat()
+    const random_items = shuffle(flatten_items)
+    const items = random_items.slice(0, 6)
+    ctx.session.place_messages = []
+    for (const { properties, geometry } of items) {
+      const place_lat = geometry.coordinates[1]
+      const place_lon = geometry.coordinates[0]
+      const loc = await ctx.replyWithLocation(place_lat, place_lon)
+      const mes = await ctx.reply(
+        properties.name + '\n' + properties.formatted,
+        Markup.inlineKeyboard([
+          Markup.button.callback('به این مورد علاقه دارم', `map_${place_lon}_${place_lat}_${lon}_${lat}`),
+        ]),
+      )
+      ctx.session.place_messages.push(loc.message_id)
+      ctx.session.place_messages.push(mes.message_id)
+    }
+    await ctx.reply('done.')
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+bot.on('message', async (ctx) => {
+  console.log(ctx)
+})
+
+bot.catch((err, ctx) => {
+  console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
+})
 bot.launch()
 
 // Enable graceful stop
---
+++ route.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+const exec = require('child_process').exec;
+const fs = require('fs')
+const shell = require('shelljs')
+
+const MAP_API_KEY = process.env.GRAPHHOPPER_TOKEN
+const MAP_ROUTE_URL =  'https://graphhopper.com/api/1/route?type=gpx&calc_points=false&key='+MAP_API_KEY
+
+const download_file = async (file_url, output_path) => {
+  const response = await axios.get(file_url)
+  fs.writeFileSync(output_path, response.data)
+}
+const make_animation = (file_name) => {
+  const script = shell.exec(`sh animate.sh ${file_name}`);
+  if (script.code !== 0) {
+    console.error(script.stdout + '\n' + script.stderr)
+    throw new Error('file convert has problem.')
+  }
+  return `files/${file_name}.mp4`
+}
+
+const route = async (source_lon, source_lat, dest_lon, dest_lat, mode, ctx) => {
+  await ctx.replyWithChatAction('record_video')
+  const url = MAP_ROUTE_URL+`&point=${source_lat},${source_lon}&point=${dest_lat},${dest_lon}&vehicle=${mode}`
+  const file_path = `${source_lon}${source_lat}${dest_lon}${dest_lat}${mode}${ctx.update.callback_query.from.id}`
+  await download_file(url, `files/${file_path}.gpx`)
+  const output_path = make_animation(file_path)
+  await ctx.replyWithChatAction('upload_video')
+  await ctx.replyWithVideo({ source: output_path })
+}
+
+module.exports = {
+  route_calculator: route,
+}
